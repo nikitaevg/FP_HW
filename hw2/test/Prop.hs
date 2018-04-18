fe4109a49935fc27a880031fe43202beeb3ba569
nikitaevg <vorobyev.nikita@gmail.com>
2018-04-18 20:18:06 +0300

    Small clarification

--- hw2/test/Prop.hs
+++ hw2/test/Prop.hs
@@ -16,6 +16,10 @@ bin_to_int = foldr (\b acc -> b + acc * 2) 0
 bins_to_int :: [[Int]] -> [Int]
 bins_to_int = map bin_to_int
 
+-- I managed to run this test only this way: stack ghci --test
+--                                           :l test/Prop.hs
+--                                           check prop_bin
+
 prop_bin :: Property
 prop_bin = property $
     forAll genInt >>= \l ->
